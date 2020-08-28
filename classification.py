cb6e56657bf6cce1a2e686a2ae6223ae3d71c8be
Ptomasetta@umass.edu <ptomasrtta@umass.edu>
2020-08-28 11:08:50 -0400

    Implemented classifier on breast-cancer dataset.

--- classification.py
+++ classification.py
@@ -1,6 +1,10 @@
 import sys
 import data_cleaner
 import numpy as np
+from sklearn.model_selection import train_test_split
+from sklearn import tree
+from sklearn import neighbors
+from sklearn.metrics import accuracy_score
 
 #############################################
 
@@ -14,11 +18,16 @@ if __name__ == '__main__':
     cleaned = data_cleaner.clean_data(dataset, target_index)
     features = cleaned[0] # M -> 1, B -> 0
     labels = cleaned[1]
-    print(len(cleaned[0]))
-    print(len(cleaned[1]))
+
     numpy_features = np.array(features)
     numpy_labels = np.array(labels)
-    print(cleaned[0])
+    
+    features_train, features_test, labels_train, labels_test = train_test_split(features, labels, test_size=.5)
+ 
+    classifier = tree.DecisionTreeClassifier()
+    classifier = neighbors.KNeighborsClassifier()
+    classifier.fit(features_train,labels_train)
 
-    
-    
+    predictions = classifier.predict(features_test)
+
+    print(accuracy_score(labels_test,predictions))
