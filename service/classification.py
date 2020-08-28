231b375a21fdd2c9af3711967c392b6e4f0e7e1b
Ptomasetta@umass.edu <ptomasrtta@umass.edu>
2020-08-28 14:30:09 -0400

    Changed classification to an internal function.

--- service/classification.py
+++ service/classification.py
@@ -1,33 +1,26 @@
 import sys
-import data_cleaner
+import data_retrieval
 import numpy as np
 from sklearn.model_selection import train_test_split
 from sklearn import tree
 from sklearn import neighbors
 from sklearn.metrics import accuracy_score
+from sklearn.metrics import confusion_matrix
 
-#############################################
+# Retrieve and clean dataset
+data = data_retrieval.retrieve_data()
 
-if __name__ == '__main__':
+# Split up data into features and labels
+x = data[0] # M -> 1, B -> 0
+y = data[1]
 
-    if(sys.argv.__len__() > 1):
-        dataset = sys.argv[1]
-    if(sys.argv.__len__() > 2):
-        target_index = sys.argv[2] # 1 for breast cancer data
-    
-    cleaned = data_cleaner.clean_data(dataset, target_index)
-    features = cleaned[0] # M -> 1, B -> 0
-    labels = cleaned[1]
+# Split the dataset into training (50%) and testing (50%)
+x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=.5)
 
-    numpy_features = np.array(features)
-    numpy_labels = np.array(labels)
-    
-    features_train, features_test, labels_train, labels_test = train_test_split(features, labels, test_size=.5)
- 
-    classifier = tree.DecisionTreeClassifier()
-    classifier = neighbors.KNeighborsClassifier()
-    classifier.fit(features_train,labels_train)
+# Build the classifier and make prediction
+classifier = tree.DecisionTreeClassifier()
+classifier = neighbors.KNeighborsClassifier()
+classifier.fit(x_train,y_train)
+predictions = classifier.predict(x_test)
 
-    predictions = classifier.predict(features_test)
-
-    print(accuracy_score(labels_test,predictions))
+print(accuracy_score(y_test,predictions))
